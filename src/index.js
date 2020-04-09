46049e87ed0a2e2802b682c4a6b726f35478074f
alan <alan.araujosb@gmail.com>
2020-04-08 23:24:18 -0300

    create register page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './_helpers'
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('app')
 );
 
 // If you want your app to work offline and load faster, you can change
